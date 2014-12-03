d4968a41acd895b934d5d952028160fb97d7178c
Geert Josten <geert.josten@marklogic.com>
2014-12-03 17:04:09 +0100

    Fixed bug in bootstrap scripts

--- opt/vagrant/bootstrap-centos-extra.sh
+++ opt/vagrant/bootstrap-centos-extra.sh
@@ -1,9 +1,6 @@
 #! /bin/sh
 echo "running $0 $@"
-if [ -d /opt/vagrant ]; then
-    cd /opt/vagrant
-fi
-sudo restore-yum-cache.sh
-sudo install-ml-CentOS.sh $2
-sudo setup-ml-extra.sh $@
-sudo backup-yum-cache.sh
+sudo /opt/vagrant/restore-yum-cache.sh
+sudo /opt/vagrant/install-ml-centos.sh $2
+sudo /opt/vagrant/setup-ml-extra.sh $@
+sudo /opt/vagrant/backup-yum-cache.sh
