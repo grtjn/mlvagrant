ce73b294933911abbd72d1dc1f6827d4dd7ca3b5
Geert Josten <geert.josten@marklogic.com>
2014-12-22 19:27:22 +0100

    Fixed #10 and #12: added install of (openjdk) java, and updated ML8 and MLCP install

--- opt/vagrant/install-mlcp.sh
+++ opt/vagrant/install-mlcp.sh
@@ -2,13 +2,22 @@
 echo "running $0 $@"
 
 yum -y install zip unzip
+yum -y install java
 
-if [ ! -d /opt/mlcp-Hadoop2-1.2-3 ]; then
-    cd /opt && unzip /space/software/mlcp-Hadoop2-1.2-3-bin.zip
-    if [ -h /usr/local/mlcp ]; then
-        unlink /usr/local/mlcp
-    fi
-fi
-if [ ! -h /usr/local/mlcp ]; then
-    cd /usr/local && ln -s /opt/mlcp-Hadoop2-1.2-3 mlcp
+if [ "$1" -eq "8" ]; then
+    echo "Installing MLCP 1.3..."
+	if [ ! -d /opt/mlcp-Hadoop2-1.3 ]; then
+	    cd /opt && unzip /space/software/mlcp-Hadoop2-1.3-bin-20141124.zip
+	fi
+	if [ ! -h /usr/local/mlcp ]; then
+	    cd /usr/local && ln -s /opt/mlcp-Hadoop2-1.3 mlcp
+	fi
+else
+    echo "Installing MLCP 1.2-3..."
+	if [ ! -d /opt/mlcp-Hadoop2-1.2-3 ]; then
+	    cd /opt && unzip /space/software/mlcp-Hadoop2-1.2-3-bin.zip
+	fi
+	if [ ! -h /usr/local/mlcp ]; then
+	    cd /usr/local && ln -s /opt/mlcp-Hadoop2-1.2-3 mlcp
+	fi
 fi
