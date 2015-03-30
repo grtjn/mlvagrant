d2413ae322b740e46c6aa2dd133208bd9f2c0ad0
Marianne Myers <marianne.gibson+gh@gmail.com>
2015-03-30 14:49:30 -0600

    #28 Add JDK to CentOS VM
    Update mulch to install the JDK rather than the JRE.  Add JAVA_HOME to
    the vagrant user’s environment.

--- opt/vagrant/bootstrap-centos-extra.sh
+++ opt/vagrant/bootstrap-centos-extra.sh
@@ -4,3 +4,4 @@ sudo /opt/vagrant/restore-yum-cache.sh
 sudo /opt/vagrant/install-ml-centos.sh $2
 sudo /opt/vagrant/setup-ml-extra.sh $@
 sudo /opt/vagrant/backup-yum-cache.sh
+sudo /opt/vagrant/set-env-vars.sh
---
+++ opt/vagrant/set-env-vars.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+echo "running $0 $@"
+
+# Vagrant Environment Variables
+echo "Setting environment variables..."
+echo "export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.75.x86_64"  >> /home/vagrant/.bashrc
