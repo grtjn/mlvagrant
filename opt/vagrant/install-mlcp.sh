0bacacae0f484fcdf08bf29700651f991cfdc99c
Patrick McElwee <pmm21@duke.edu>
2015-04-14 15:42:26 -0400

    Upgrade to MarkLogic 8.0-2

--- opt/vagrant/install-mlcp.sh
+++ opt/vagrant/install-mlcp.sh
@@ -5,12 +5,12 @@ yum -y install zip unzip
 yum -y install java
 
 if [ "$1" -eq "8" ]; then
-    echo "Installing MLCP 1.3-1..."
-	if [ ! -d /opt/mlcp-Hadoop2-1.3-1 ]; then
-	    cd /opt && unzip /space/software/mlcp-Hadoop2-1.3-1-bin.zip
+    echo "Installing MLCP 1.3-2..."
+	if [ ! -d /opt/mlcp-Hadoop2-1.3-2 ]; then
+	    cd /opt && unzip /space/software/mlcp-Hadoop2-1.3-2-bin.zip
 	fi
 	if [ ! -h /usr/local/mlcp ]; then
-	    cd /usr/local && ln -s /opt/mlcp-Hadoop2-1.3-1 mlcp
+	    cd /usr/local && ln -s /opt/mlcp-Hadoop2-1.3-2 mlcp
 	fi
 else
     echo "Installing MLCP 1.2-3..."
