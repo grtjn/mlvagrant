7fee759654e1f676ce0c8f02d7b045e1e48b64f6
David Grant <dave.grant@marklogic.com>
2015-04-20 16:17:49 -0400

    #36 - Fixed file version for ML7 and MLCP (for ML7 and earlier).

--- opt/vagrant/install-mlcp.sh
+++ opt/vagrant/install-mlcp.sh
@@ -12,12 +12,20 @@ if [ "$1" -eq "8" ]; then
 	if [ ! -h /usr/local/mlcp ]; then
 	    cd /usr/local && ln -s /opt/mlcp-Hadoop2-1.3-2 mlcp
 	fi
+elif [ "$1" -eq "7" ]; then
+    echo "Installing MLCP 1.2-4..."
+  if [ ! -d /opt/mlcp-Hadoop2-1.2-4 ]; then
+      cd /opt && unzip /space/software/mlcp-Hadoop2-1.2-4-bin.zip
+  fi
+  if [ ! -h /usr/local/mlcp ]; then
+      cd /usr/local && ln -s /opt/mlcp-Hadoop2-1.2-4 mlcp
+  fi
 else
-    echo "Installing MLCP 1.2-3..."
-	if [ ! -d /opt/mlcp-Hadoop2-1.2-3 ]; then
-	    cd /opt && unzip /space/software/mlcp-Hadoop2-1.2-3-bin.zip
+    echo "Installing MLCP 1.0-5..."
+	if [ ! -d /opt/mlcp-Hadoop2-1.0-5 ]; then
+	    cd /opt && unzip /space/software/mlcp-Hadoop2-1.0-5-bin.zip
 	fi
 	if [ ! -h /usr/local/mlcp ]; then
-	    cd /usr/local && ln -s /opt/mlcp-Hadoop2-1.2-3 mlcp
+	    cd /usr/local && ln -s /opt/mlcp-Hadoop2-1.0-5 mlcp
 	fi
 fi
