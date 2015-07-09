1934720c5d79085d8b91dc2f3fca4d88af9d3458
Dave Grant <Dave.Grant@marklogic.com>
2015-07-09 15:28:20 -0400

    #39 - Defined configurable properties for ML installer and MLCP installer.

--- opt/vagrant/bootstrap-centos-extra.sh
+++ opt/vagrant/bootstrap-centos-extra.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 echo "running $0 $@"
+
+# Convert all property keys to lowercase and store to tmp file to be sourced.
+sed 's/.*=/\L&/' /vagrant/project.properties > /tmp/mlvagrant.project.properties
+
+# Run the installers
 sudo /opt/vagrant/restore-yum-cache.sh
-sudo /opt/vagrant/install-ml-centos.sh $2
+sudo /opt/vagrant/install-ml-centos.sh
 sudo /opt/vagrant/setup-ml-extra.sh $@
 sudo /opt/vagrant/backup-yum-cache.sh
