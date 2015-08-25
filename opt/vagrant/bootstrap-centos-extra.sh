6ec81ac4d99307a30c4f2f0e894f85f9c6a3d519
Geert Josten <geert.josten@marklogic.com>
2015-08-25 21:25:07 +0200

    Fixed #52: allow running scripts stand-alone

--- opt/vagrant/bootstrap-centos-extra.sh
+++ opt/vagrant/bootstrap-centos-extra.sh
@@ -2,10 +2,14 @@
 echo "running $0 $@"
 
 # Convert all property keys to lowercase and store to tmp file to be sourced.
-sed 's/.*=/\L&/' /vagrant/project.properties > /tmp/mlvagrant.project.properties
+if [ -d /vagrant ]; then
+  sed 's/.*=/\L&/' /vagrant/project.properties > /tmp/$4.project.properties
+else
+  printf "ml_version=$2\n" > /tmp/$4.project.properties
+fi
 
 # Run the installers
 sudo /opt/vagrant/restore-yum-cache.sh
-sudo /opt/vagrant/install-ml-centos.sh
+sudo /opt/vagrant/install-ml-centos.sh $2
 sudo /opt/vagrant/setup-ml-extra.sh $@
 sudo /opt/vagrant/backup-yum-cache.sh
--- opt/vagrant/bootstrap-centos-master.sh
+++ opt/vagrant/bootstrap-centos-master.sh
@@ -2,14 +2,18 @@
 echo "running $0 $@"
 
 # Convert all property keys to lowercase and store to tmp file to be sourced.
-sed 's/.*=/\L&/' /vagrant/project.properties > /tmp/mlvagrant.project.properties
+if [ -d /vagrant ]; then
+  sed 's/.*=/\L&/' /vagrant/project.properties > /tmp/$4.project.properties
+else
+  printf "ml_version=$2\n" > /tmp/$4.project.properties
+fi
 
 # Run the installers.
 sudo /opt/vagrant/restore-yum-cache.sh
-sudo /opt/vagrant/install-ml-centos.sh
+sudo /opt/vagrant/install-ml-centos.sh $4
 sudo /opt/vagrant/setup-ml-master.sh $1 $2 $3
 sudo /opt/vagrant/install-node.sh
-sudo /opt/vagrant/install-mlcp.sh
+sudo /opt/vagrant/install-mlcp.sh $4
 sudo /opt/vagrant/install-user.sh $4
 sudo /opt/vagrant/setup-git.sh $4
 sudo /opt/vagrant/setup-tomcat.sh
