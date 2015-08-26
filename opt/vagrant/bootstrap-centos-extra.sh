dbeddcd71fdcd32a81691a68c483ed1a3c71a450
Geert Josten <geert.josten@marklogic.com>
2015-08-26 16:28:49 +0200

    Fixed #65: typo in bootstrap extra

--- opt/vagrant/bootstrap-centos-extra.sh
+++ opt/vagrant/bootstrap-centos-extra.sh
@@ -10,6 +10,6 @@ fi
 
 # Run the installers
 sudo /opt/vagrant/restore-yum-cache.sh
-sudo /opt/vagrant/install-ml-centos.sh $2
+sudo /opt/vagrant/install-ml-centos.sh $4
 sudo /opt/vagrant/setup-ml-extra.sh $@
 sudo /opt/vagrant/backup-yum-cache.sh
