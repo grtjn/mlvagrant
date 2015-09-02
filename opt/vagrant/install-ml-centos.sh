de615f934cf52d58008b434c07072e1b52676c33
Geert Josten <geert.josten@marklogic.com>
2015-09-02 13:23:45 +0200

    Fixed #44: updated default ml installers to 6.0-6 and 7.0-5.2

--- opt/vagrant/install-ml-centos.sh
+++ opt/vagrant/install-ml-centos.sh
@@ -40,11 +40,11 @@ if [ -n "${ml_installer}" ]; then
 elif [ "${ml_version}" = "5" ]; then
     installer="MarkLogic-5.0-6.1.x86_64.rpm"
 elif [ "${ml_version}" = "6" ]; then
-    installer="MarkLogic-6.0-5.3.x86_64.rpm"
+    installer="MarkLogic-6.0-6.x86_64.rpm"
 elif [ "${ml_version}" = "8" ]; then
     installer="MarkLogic-8.0-3.x86_64.rpm"
 else
-    installer="MarkLogic-7.0-5.1.x86_64.rpm"
+    installer="MarkLogic-7.0-5.2.x86_64.rpm"
 fi
 
 # Run MarkLogic installer
