d1126229986fee02db6a70895509ac1460bb1302
Geert Josten <geert.josten@marklogic.com>
2017-06-26 20:28:09 +0200

    Fixed #100: install of latest node now default

--- opt/vagrant/install-node.sh
+++ opt/vagrant/install-node.sh
@@ -3,6 +3,7 @@ echo "running $0 $@"
 
 # Defaults
 install_nodejs=true
+install_nodejs_lts=true
 install_ruby=true
 
 # Load the normalized project properties.
@@ -10,8 +11,20 @@ source /tmp/$1.project.properties
 
 if [ $install_nodejs == "true" ]; then
 
-  yum -y install curl epel-release # epel-release necessary to install nodejs/npm on CentOS 5, and no harm otherwise
-  curl --silent --location https://rpm.nodesource.com/setup | bash -
+  # make sure nodejs is not pre-installed
+  yum remove -y nodejs npm
+
+  # curl, as well as epel-release necessary to install nodejs/npm on CentOS 5, no harm otherwise
+  yum -y install curl epel-release 
+
+  # get correct nodejs installation
+  if [ $install_nodejs_lts == "true" ]; then
+    curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash -
+  else
+    curl --silent --location https://rpm.nodesource.com/setup | bash -
+  fi
+
+  # install nodejs + potentially necessary build tools
   yum -y install gcc-c++ make nodejs
 
   if [ hash bower 2> /dev/null ]; then
